334e305dc7f8044c4988995b86a970ec1ad82157
cbobowic <colden_bobowick@brown.edu>
2024-10-06 13:04:59 -0400

    updating with QUEST

--- Matlab Control System/cubesatSimSample2/test.m
+++ Matlab Control System/cubesatSimSample2/test.m
@@ -1,10 +1,16 @@
 
-disp(testing(1))
+p = [0.1,0.1,0.8; 0.1,0.8,0.1]';
+q = [0,0, 1; 0,1,0]';
 
-function test = testing(in)
-    
-    test = in;
-    out = 1;
-    disp(out);
+% disp(p);
+% disp(size(p));
+% % disp(p(:,1));
 
-end
+[R t err] = quest(p,q);
+
+disp(R*p);
+% disp(R*q);
+
+% disp(R);
+disp("Error");
+disp(err);
